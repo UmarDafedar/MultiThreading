5dcaee6707694c4b14ad04b555fffaf2b78cf573
Umar Dafedar <100466050+UmarDafedar@users.noreply.github.com>
2022-03-11 20:59:09 +0530

    Create lock_And_Try_Lock_withinSameFunction.cpp
    Case 2: Same thread is locked and again trying to lock using try_lock, try_lock_for,try_lock_until in mutex and timed_mutex

---
+++ lock_And_Try_Lock_withinSameFunction.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+#include<chrono>
+#include<ctime>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+mutex m1;
+// lock()
+// unlock()
+// try_lock()
+
+#define COUNTER_LIMIT 20
+int counter=COUNTER_LIMIT;
+
+void PrintFunction(int i)
+{
+ cout<<"Zzzzzzzzzzzz"<<endl;
+ m1.lock();
+ if(m1.try_lock())
+ {
+  cout<<"Lock is acquired"<<endl;
+  if(i<=0)
+   return;
+  else  if(i==1)
+   cout<<i<<endl;
+  else
+  {
+   PrintFunction(i-1);
+   cout<<i<<endl;
+  }
+  
+  m1.unlock();
+ }
+ else
+ {
+  cout<<"The lock is not acquired"<<endl;
+  return;
+ }
+}
+
+int main()
+{
+ thread t1(PrintFunction,10);
+ t1.join();
+ return 0;
+}
