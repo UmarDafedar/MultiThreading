8fe5160a70afc698e6b72dadbafb9a7d2e37a7a2
Umar Dafedar <100466050+UmarDafedar@users.noreply.github.com>
2022-03-22 11:35:26 +0530

    Create lock_guard_NotCopyable_NotMovable.cpp
    lock_guard is always engaged; it always holds a reference to a mutex and always unlocks it in its destructor. If it was movable then it would need to hold a pointer instead of a reference, and test the pointer in its destructor. This might seem a trivial cost, but it is C++ philosophy that you don't pay for what you don't use.
    
    If you want a movable (and releaseable) lock you can use unique_lock.
    
    
    template <typename T> class lock_guard_
    {
      T* Mutex_;
      lock_guard_(const lock_guard_&) = delete;
      lock_guard_& operator=(const lock_guard_&) = delete;
    public:
      lock_guard_(T& mutex) : Mutex_(&mutex)
      {
        Mutex_->lock();
      }
      ~lock_guard_()
      {
        if(Mutex_!=nullptr)
          Mutex_->unlock();
      }
      lock_guard_(lock_guard_&& guard)
      {
        Mutex_ = guard.Mutex_;
        guard.Mutex_ = nullptr;
      }
    };
    
    template <typename T> lock_guard_<T> lock_guard(T& mutex)
    {
      return lock_guard_<T>(mutex);
    }

---
+++ lock_guard_NotCopyable_NotMovable.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<mutex>
+#include<shared_mutex>
+#include<thread>
+
+using namespace std;
+
+mutex m1;
+
+void LockingFunctn()
+{
+ m1.lock();
+ cout<<"Lock is acquired in thread"<<endl;
+ this_thread::sleep_for(10s);
+ m1.unlock();
+ cout<<"Lock is released in thread"<<endl;
+}
+
+int main()
+{
+ // thread t1(LockingFunctn);
+ // this_thread::sleep_for(2s);
+ //m1.lock();
+ lock_guard<mutex> lock(m1,std::adopt_lock);// does not lock
+ cout<<"Lock is not acquried as I am using adopt_lock strategy"<<endl;
+ this_thread::sleep_for(2s);
+ // lock_guard<mutex> lock2(m1);
+ thread t1(LockingFunctn);
+ cout<<"Lock is acquried"<<endl;
+ t1.join();
+ return 0;
+}
